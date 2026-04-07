123e873f8d1bbf0adfff226b631f8c302ac7e914
ALP300 <aitorpalomino1@gmail.com>
2026-04-07 17:27:33 -0500

    a

--- src/main/java/com/eccomerce/backend/controller/ProductController.java
+++ src/main/java/com/eccomerce/backend/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,9 +38,15 @@ public class ProductController {
     }
 
     @PostMapping
-    @Operation(summary = "Crear o actualizar producto", description = "Guarda la información de un nuevo producto o actualiza uno existente")
+    @Operation(summary = "Crear producto", description = "Guarda la información de un nuevo producto")
     public Product saveProduct(@RequestBody Product product) {
         return productService.saveProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Actualizar producto", description = "Actualiza la información de un producto existente según su ID")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
     }
 
     @DeleteMapping("/{id}")
